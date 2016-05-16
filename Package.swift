f5775919ea591b6bb8464ec8a478dbdd22407b9c
Paulo Faria <paulo.faria.rl@gmail.com>
2016-05-16 16:09:53 -0300

    use proper name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-apex",
+    name: "Apex",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 5),
