ffa72bd3291c9060a9fbffe9f282ca3033901484
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-05-18 18:28:39 +0100

    Switched to streaming of data in, instead of loading everything at once

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/czechboy0/Jay.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/czechboy0/Jay.git", majorVersion: 0, minor: 8),
     ]
 )
